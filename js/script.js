8d252809cd58eeeb538996e6a1d09e5c87ee031f
vladimir2809 <46346862+vladimir2809@users.noreply.github.com>
2024-07-19 16:34:18 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ var canvasHeight = 600;
 var size = 40;
 var quantityColor = 64;
 var quantityPanzer = 64;
+var quantityWall = 64;
 var direction = 0;
+var numSelectPanzer = 0;
 var panzer = {
     being: true,
     x:1,
@@ -16,14 +18,39 @@ var panzer = {
     color: 'white',
     angleBody: 0,
     angleTower: 0,
+    dir:0,
+    speed:10,
     towerX:null,
     towerY: null,
     towerX1: null,
     towerY1: null,
     towerLength: 10,
 }
+var wall = {
+    x:null,
+    y:null,
+    width: size,
+    height: size,
+    type: 0,
+    color: 'grey',
+}
+var map = {
+    x:0,
+    y:0,
+    width: 800,
+    height: 600,
+}
+var camera = {
+    x:0,
+    y:0,
+    width: 800,
+    height: 600, 
+}
+
 var colorArr = [];
-panzerArr = [];
+var panzerArr = [];
+var wallArr = [];
+
 window.addEventListener('load', function () {
     preload();
     create();
@@ -34,12 +61,14 @@ window.addEventListener('load', function () {
 });
 function preload() 
 {
-
+    
 }
 function create()
 {
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
+    initKeyboardAndMouse(['KeyA', 'KeyW', 'KeyS', 'KeyD']);
+    srand(2);
     // создаем список цветов в градиенте от красновго до синего через зеленый
     for (let i = 0; i < quantityColor;i++)
     {
@@ -72,19 +101,36 @@ function create()
         colorArr.push(color);
     }
     console.log(colorArr);
+    for (let i = 0; i < quantityWall;i++)
+    {
+        let wallOne=JSON.parse(JSON.stringify(wall));
+        let x = randomInteger(0,Math.trunc(map.width / wallOne.width)-1);
+        let y = randomInteger(0,Math.trunc(map.height / wallOne.width)-1);;
+        wallOne.x = x * wallOne.width;
+        wallOne.y = y * wallOne.width;
+        wallArr.push(wallOne);
+    }
     // инициализиуем танки
     for (let i = 0; i < quantityPanzer;i++)
     {
         let panzerOne = JSON.parse(JSON.stringify(panzer));
-        panzerOne.x = panzerOne.width * ((i % 8))*1.5;
-        panzerOne.y = panzerOne.height * Math.trunc((i / 8))*1.5;
+        let flag = false;
+        do {
+            let x = randomInteger(0,Math.trunc(map.width / wall.width)-1);
+            let y = randomInteger(0,Math.trunc(map.height / wall.width)-1);;
+            panzerOne.x = x * wall.width;
+            panzerOne.y = y * wall.width;
+ 
+        } while (collisionPanzerWall(panzerOne) == true ||
+            collisionPanzerToPanzer(panzerOne,null) == true);
         let index = i % quantityColor;
         panzerOne.color = colorArr[index];
         
         panzerArr.push(panzerOne);
         updateStatePanzer(panzerArr[i]);
-
     }
+
+    //console.log(wallArr);
 }
 function drawAll() 
 {
@@ -92,16 +138,41 @@ function drawAll()
     context.fillRect(0, 0, canvasWidth, canvasHeight);
     for (let i = 0; i < quantityPanzer;i++)
     {
-        drawPanzer(panzerArr[i]);
+        if (numSelectPanzer==i)
+        {
+            drawPanzer(panzerArr[i],true);
+
+        }
+        else
+        {
+            drawPanzer(panzerArr[i]);
+        }
+    }
+    for (let i = 0; i < quantityWall;i++)
+    {
+        drawWall(wallArr[i]);
     }
     
 }
-function drawPanzer(panzer)
+function drawWall(wall)
+{
+    context.fillStyle = wall.color;
+    context.fillRect(wall.x, wall.y, wall.width, wall.height);
+}
+function drawPanzer(panzer,select=false)
 {
     var multSide=0.15;
 
 
-    context.strokeStyle=panzer.color;
+    if (select) 
+    {
+        context.strokeStyle = 'white';
+    }
+    else
+    {
+        context.strokeStyle=panzer.color;
+    }
+    
     // врашаем тело танка
     context.save();
   
@@ -109,7 +180,7 @@ function drawPanzer(panzer)
 
 
 
-    context.rotate((Math.PI / 180) * panzer.angleBody/*Math.trunc(panzer.angleBody/90)*90*/); // rotate
+    context.rotate((Math.PI / 180) * (panzer.angleBody+90)/*Math.trunc(panzer.angleBody/90)*90*/); // rotate
 
     context.translate(-(panzer.x + panzer.width / 2), -(panzer.y + panzer.height / 2)); // translate back
 
@@ -130,7 +201,6 @@ function drawPanzer(panzer)
     
     // рисуем пушку
     context.beginPath();
-    context.strokeStyle=panzer.color;
     context.lineWidth = 3;
     context.moveTo(panzer.towerX,panzer.towerY);
     context.lineTo(panzer.towerX1,panzer.towerY1);
@@ -140,12 +210,20 @@ function update()
 {
     for (let i = 0; i < quantityPanzer;i++)
     {
-        panzerArr[i].angleBody++;
-        panzerArr[i].angleTower-=4;
-        if (panzerArr[i].x == 800 && direction==0) direction = 1;
+        //panzerArr[i].angleBody++;
+        //panzerArr[i].angleTower-=4;
+       /* if (panzerArr[i].x == 800 && direction==0) direction = 1;
         if (panzerArr[i].x == 1 && direction==1) direction =0;
         if (direction == 0) panzerArr[i].x++;
-        if (direction == 1) panzerArr[i].x--;
+        if (direction == 1) panzerArr[i].x--;*/
+        if (numSelectPanzer==i)
+        {
+            controlHumanPanzer(panzerArr[i]);
+            collisionPanzerWall(panzerArr[i]);
+            collisionRectangleMap(panzerArr[i]);
+            collisionPanzerToPanzer(panzerArr[i],i)
+          
+        }
         updateStatePanzer(panzerArr[i]);
     }
 }
@@ -157,4 +235,123 @@ function updateStatePanzer(panzer)
     panzer.towerX = centerX + Math.cos((Math.PI / 180) * panzer.angleTower) * panzer.sizeTower;
     panzer.towerY1 = panzer.towerY + Math.sin((Math.PI / 180) * panzer.angleTower) * panzer.towerLength;
     panzer.towerX1 = panzer.towerX + Math.cos((Math.PI / 180) * panzer.angleTower) * panzer.towerLength;
+}
+function controlHumanPanzer(panzer)
+{
+    if (checkPressKey('KeyW') == true && panzer.dir!=0) {panzer.dir=0; panzer.angleTower=panzer.angleBody=270}
+    if (checkPressKey('KeyD') == true && panzer.dir!=1) {panzer.dir=1; panzer.angleTower=panzer.angleBody=0}
+    if (checkPressKey('KeyS') == true && panzer.dir!=2) {panzer.dir=2; panzer.angleTower=panzer.angleBody=90}
+    if (checkPressKey('KeyA') == true && panzer.dir!=3) {panzer.dir=3; panzer.angleTower=panzer.angleBody=180}
+
+    if (checkPressKey('KeyW') == true && panzer.dir==0) panzer.y-=panzer.speed;
+    else if (checkPressKey('KeyD') == true && panzer.dir==1) panzer.x+=panzer.speed;
+    else if (checkPressKey('KeyS') == true && panzer.dir==2) panzer.y+=panzer.speed;
+    else if (checkPressKey('KeyA') == true && panzer.dir==3) panzer.x-=panzer.speed;
+
+   
+}
+function collisionPanzerWall(panzer)
+{
+    for (let i = 0; i < quantityWall;i++)
+    {
+        if (panzer.x+panzer.width>wallArr[i].x && 
+            panzer.x<wallArr[i].x+wallArr[i].width &&
+            panzer.y+panzer.height>wallArr[i].y && 
+            panzer.y<wallArr[i].y+wallArr[i].height 
+            )
+        {
+            if ( panzer.dir==0)
+            {
+                panzer.y = wallArr[i].y + wallArr[i].height + 2;
+                return true;
+            }
+            if ( panzer.dir==2)
+            {
+                panzer.y = wallArr[i].y - panzer.height - 2;
+                return true;;
+            }
+
+            
+           
+            if (panzer.dir==3)
+            {
+                panzer.x = wallArr[i].x + wallArr[i].width + 1;
+                return true;;
+            }
+            if  (panzer.dir==1)
+            {
+                panzer.x = wallArr[i].x - panzer.width - 1;
+                return true;;
+            }
+
+           
+        }
+
+    }
+    return false;   
+}
+function collisionRectangleMap(panzer)
+{
+    if (panzer.x<map.x)
+    {
+        panzer.x = map.x+1;
+        return true;
+    }
+    if (panzer.x+panzer.width>map.x+map.width)
+    {
+        panzer.x = map.x+map.width-panzer.width-1;
+        return true;
+    }
+
+    if (panzer.y<map.y)
+    {
+        panzer.y = map.y+1;
+        return true;
+    }
+    if (panzer.y+panzer.height>map.y+map.height)
+    {
+        panzer.y = map.y+map.height-panzer.height-1;
+        return true;
+    }
+    return false;
+}
+function collisionPanzerToPanzer(panzer,num)
+{
+    for (let i = 0; i < panzerArr.length;i++)
+    {
+        if (num!=i)
+        {
+            console.log(panzerArr[i]);
+            if (panzer.x+panzer.width>panzerArr[i].x &&
+                panzer.x<panzerArr[i].x+panzerArr[i].width &&
+                panzer.y+panzer.height>panzerArr[i].y &&
+                panzer.y<panzerArr[i].y+panzerArr[i].height )
+            {
+                if ( panzer.dir==0)
+                {
+                    panzer.y = panzerArr[i].y + panzerArr[i].height + 2;
+                    return true;
+                }
+                if ( panzer.dir==2)
+                {
+                    panzer.y = panzerArr[i].y - panzer.height - 2;
+                    return true;;
+                }
+
+            
+           
+                if (panzer.dir==3)
+                {
+                    panzer.x = panzerArr[i].x + panzerArr[i].width + 1;
+                    return true;;
+                }
+                if  (panzer.dir==1)
+                {
+                    panzer.x = panzerArr[i].x - panzer.width - 1;
+                    return true;;
+                }
+            }
+        }
+    }
+    return false;
 }
